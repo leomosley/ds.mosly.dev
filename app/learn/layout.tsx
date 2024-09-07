3ef5b2b380a4a162b63bcd09e4b68899d2a4e659
leomosley <leo.mosley2@gmail.com>
2024-09-07 17:01:32 +0100

    feat: add sidebar

---
+++ app/learn/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "@/components/sidebar";
+
+export default function LearnLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main className="flex gap-2 h-screen items-start">
+      <Sidebar />
+      <article className="flex flex-col flex-grow p-4">
+        {children}
+      </article>
+    </main>
+  );
+}
---
+++ components/sidebar.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export function Sidebar() {
+  return (
+    <aside className="sticky h-screen border-r p-4 bg-muted/20 top-0 sm:w-1/6 hidden sm:block">
+    </aside>
+  );
+}
